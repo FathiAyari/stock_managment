1115b196c8a0be80a3fdd5daf81d1186a8dc9202
FathiAyari <fathi.ayari@kelibia.r-iset.tn>
2023-06-07 01:47:10 +0200

    delete product

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -43,7 +43,7 @@ class ProductController extends Controller
      */
     public function destroy($id )
     {
-        $product=Client::find($id);
+        $product=Product::find($id);
 
         $product->delete();
         return response([
