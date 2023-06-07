132421f65056ba77ad02cae54e4abfa49a6dedac
FathiAyari <fathi.ayari@kelibia.r-iset.tn>
2023-06-07 05:38:12 +0200

    get recap data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
+use App\Models\Product;
+use App\Models\Sale;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
+        $products=Product::all()->count();
+        $clients=Client::all()->count();
+        $currentDate = Carbon::now()->toDateString();
+        $sum = Sale::whereDate('created_at', $currentDate)->sum('price');
+        return response()->json( ["products"=>$products,"clients"=>$clients,"sum"=>$sum], 200);
 
     }
 }
